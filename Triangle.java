c42dad9f2bffa6e0cf8c462a9c36156d5b068c78
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-10-18 13:17:54 -0400

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,33 @@
+/** Class: Triangle.java
+*@author Adrian Moreno
+* Course: ITEC 2150
+* Written: 10/18/2022
+*
+* This class – This is the Triangle class. Contains a method that checks if a triangle is a true triangle.
+*/
+public class Triangle 
+{
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1, double side2, double side3) 
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+
+    public void checkSides()throws IllegalTriangleSideException
+    {
+        if(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
+        {
+            System.out.println("This is a triangle.");
+
+        }
+        else
+        {
+            throw new IllegalTriangleSideException("Error! This is not a triangle. Please try again.");
+        }
+    }
