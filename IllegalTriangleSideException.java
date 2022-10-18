b69377ce1443c72e12beacb6bbf8b02cd3852014
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-10-18 13:21:46 -0400

    Create IllegalTriangleSideException.java

---
+++ IllegalTriangleSideException.java
@@ -0,0 +1,8 @@
+public class IllegalTriangleSideException extends Exception
+{
+    public IllegalTriangleSideException()
+    {
+         System.out.println("Error! This is not a triangle. Please try again.");
+    }
+
+  
