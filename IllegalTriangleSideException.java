d61dd6935e8320726166de67651922c0f41773f8
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-10-18 13:37:13 -0400

    Update IllegalTriangleSideException.java

--- IllegalTriangleSideException.java
+++ IllegalTriangleSideException.java
@@ -1,3 +1,10 @@
+/** Class: IllegalTriangleSideException.java
+*@author Adrian Moreno
+* Course: ITEC 2150
+* Written: 10/18/2022
+*
+* This class – Creates a custom Exception for the triangle class.
+*/
 public class IllegalTriangleSideException extends Exception
 {
     public IllegalTriangleSideException()
